9b8bf8969a4a0a93beb9de6b91cb3dd024a74d1d
sH4MbLe5 <110418075+sH4MbLe5@users.noreply.github.com>
2023-05-15 17:29:17 +0200

    Delete datamodel_b07_tc directory

--- datamodel_b07_tc/__init__.py
+++
@@ -1,3 +0,0 @@
-
-__URL__ = "https://github.com/FAIRChemistry/datamodel_b07_tc.git"
-__COMMIT__ = "e824f185724b28508edb41513f76000bd0fe3798"
--- datamodel_b07_tc/core/role.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Role(Enum):
-    EDUCT = "Educt"
-    PRODUCT = "Product"
-    CATALYST = "Catalyst"
-    SOLVENT = "Solvent"
-    INERTGAS = "Inert Gas"
