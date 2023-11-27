8872c248623884be3e946849d19313d400c3d949
samirdarouich <80834921+samirdarouich@users.noreply.github.com>
2023-11-27 16:23:13 +0100

    Delete datamodel_b07_tc directory

--- datamodel_b07_tc/__init__.py
+++
@@ -1,3 +0,0 @@
-
-__URL__ = "https://github.com/FAIRChemistry/datamodel_b07_tc.git"
-__COMMIT__ = "0730e4039ae52cfaf41cc2b4bea63a848f91ced6"
--- datamodel_b07_tc/core/thermocoupletype.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class ThermocoupleType(Enum):
-    JTYPE = "Type J"
-    KTYPE = "Type K"
