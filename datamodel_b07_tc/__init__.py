760812c383c673e124cfc79ccf246fbf0b92c7e5
samirdarouich <samirdar@web.de>
2024-01-09 07:24:07 +0100

    t

--- datamodel_b07_tc/__init__.py
+++
@@ -1,3 +0,0 @@
-
-__URL__ = "https://github.com/FAIRChemistry/FAIRFlowChemistry.git"
-__COMMIT__ = "1bfa14eeb39e4bc915f2ec015be812f5dd1b4bb7"
--- datamodel_b07_tc/core/measurementtype.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class MeasurementType(Enum):
-    POTENTIOSTATIC = "Potentiostatic measurement"
-    GALVANOSTATIC = "Galvanostatic measurement"
-    GC = "GC measurement"
-    MFM = "MFM measurement"
